5683ee95df34d6126108bc0ebf07dfa7abc44ad0
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-11-08 04:00:35 +0600

    add quary and filter in product route for load data email wase

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,76 +1,129 @@
 const { createProductService, getProductService, getProductByIdService, updateProductService } = require("../services/product.service")
 
-exports.createProduct=async (req, res, next) => {
-    try {
-      //create method
-      const result=await createProductService(req.body)
-  
-      res.status(200).json({
-        stauts: "success",
-        massage: "successfully create a Product",
-        data: result
-      })
-    } catch (error) {
-      res.status(400).json({
-        stauts:"fail",
-        message: "Data is not inserted",
-        error : error.message
-      })
-    }  
-  }
+exports.createProduct = async (req, res, next) => {
+  try {
+    //create method
+    const result = await createProductService(req.body)
 
-  exports.getProduct=async (req, res) => {
-    try {
-      //create method
-      const record=await getProductService();
-      res.send(record)
-    } catch (error) {
-      res.status(400).json({
-        stauts:"fail",
-        message: "Data is not found",
-        error : error.message
-      })
-  
+    res.status(200).json({
+      stauts: "success",
+      massage: "successfully create a Product",
+      data: result
+    })
+  } catch (error) {
+    res.status(400).json({
+      stauts: "fail",
+      message: "Data is not inserted",
+      error: error.message
+    })
+  }
+}
+
+exports.getProduct = async (req, res) => {
+  /*  try {
+     let filters={...req.query}
+     //solt,page,limit, --- exclude
+     const excludeField=['sort','page','limit']
+     excludeField.forEach(field =>delete filters[field])
+
+      //gt,li,get,lte
+      let filterString=JSON.stringify(filters)
+      filterString=filterString.replace(/\b(gt|gte|lt|Lte)\b/g, match => `$${match}`)
+ 
+      filters=JSON.parse(filterString)
+      
+      if(req.query.fields){
+       const fields=req.query.fields.split(',').join(' ')
+       queries.fields=fields
+       console.log(fields)
+     }
+     if(req.query.page){
+       const{page=1,limit=10}=req.query;
+       const skip=(page-1)*parseInt(limit);
+       queries.skip=skip;
+       queries.limit=parseInt(limit);
+     }
+     const record=await getProductService(filters,queries);
+     res.send(record)
+   } catch (error) {
+     res.status(400).json({
+       stauts:"fail",
+       message: "Data is not found",
+       error : error.message
+     })
+ 
+   } */
+  try {
+    //find by id
+    let filters = { ...req.query }
+    //solt,page,limit, --- exclude
+    const excludeField = ['sort', 'page', 'limit']
+    excludeField.forEach(field => delete filters[field])
+
+    //gt,li,get,lte
+    let filterString = JSON.stringify(filters)
+    filterString = filterString.replace(/\b(gt|gte|lt|Lte)\b/g, match => `$${match}`)
+
+    filters = JSON.parse(filterString)
+
+    //
+    const queries = {}
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ')
+      queries.fields = fields
     }
+    const product = await getProductService(filters, queries);
+    res.status(200).json({
+      stauts: "success",
+      massage: "successfully get data for all Product",
+      data: product
+    })
+  } catch (error) {
+    res.status(400).json({
+      stauts: "fail",
+      message: "Data is not found",
+      error: error.message
+    })
   }
+}
 
-  exports.getProductById=async (req, res, next) => {
-    const {id}=req.params;
-    try {
-      //create method
-      const product=await getProductByIdService(id);
-      if(!product){
-        return res.status(400).json({
-            stauts:"fail",
-            error : "Could not finds a brand with this id"
-          })
-      }
-      res.send(product)
-    } catch (error) {
-      res.status(400).json({
-        stauts:"fail",
-        message: "Data is not inserted",
-        error : error.message
+exports.getProductById = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    //create method
+    const product = await getProductByIdService(id);
+    if (!product) {
+      return res.status(400).json({
+        stauts: "fail",
+        error: "Could not finds a brand with this id"
       })
-    }  
-  }
-
-  exports.updateProduct=async (req, res, next) => {
-    const {id}=req.params;
-    try {
-      const result =await updateProductService(id,req.body);
-     
-      res.status(200).json({
-        stauts: "success",
-        massage: "successfully update the quantity",
-      })
-    } catch (error) {
-      res.status(400).json({
-        stauts:"fail",
-        message: "Could not update",
-        error : error.message
-      })
-  
     }
-    
+    res.send(product)
+  } catch (error) {
+    res.status(400).json({
+      stauts: "fail",
+      message: "Data is not inserted",
+      error: error.message
+    })
   }
+}
+
+exports.updateProduct = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const result = await updateProductService(id, req.body);
+
+    res.status(200).json({
+      stauts: "success",
+      massage: "successfully update the quantity",
+    })
+  } catch (error) {
+    res.status(400).json({
+      stauts: "fail",
+      message: "Could not update",
+      error: error.message
+    })
+
+  }
+
+}
--- services/product.service.js
+++ services/product.service.js
@@ -4,8 +4,14 @@ exports.createProductService = async (data) => {
     const result = await Product.create(data)
     return result;
 }
-exports.getProductService = async () => {
-    const result = await Product.find({})
+exports.getProductService = async (filters,queries) => {
+    /* const products = await Product.find(filters).select(queries.fields)
+    return products; */
+    const result = await Product.find(filters)
+    .skip(queries.skip)
+    .limit(queries.limit)
+    .sort(queries.sortBy)
+    .select(queries.fields)
     return result;
 }
 exports.getProductByIdService = async (id) => {
